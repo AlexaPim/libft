242a2b46fb19127c2eb8a5257a29658de908825e
ado-prad <ado-prad@student.42sp.org.br>
2022-09-22 20:20:46 +0200

    up

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: ado-prad <ado-prad@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 19:43:24 by ado-prad          #+#    #+#             */
-/*   Updated: 2022/09/22 19:57:04 by ado-prad         ###   ########.fr       */
+/*   Updated: 2022/09/22 20:18:28 by ado-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strdup(const char *s)
 	int		i;
 
 	size = ft_strlen(s);
+	if (!s)
+		return (NULL);
 	string = malloc(sizeof(char) * (size + 1));
 	i = 0;
 	while (s[i] != '\0')
